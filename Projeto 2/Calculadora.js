c6f0e6add8ae893292dd6d16d2887ea3ea18ddae
SamuelFM147 <samumichels@gmail.com>
2025-03-31 10:55:34 -0300

    Calculadora Finalizada

--- Projeto 2/Calculadora.js
+++ Projeto 2/Calculadora.js
@@ -1,15 +1,33 @@
+let resultCalculated = false
+
+
+
 function addToDisplay(value) {
+    if(resultCalculated){
+        clearDisplay();
+        resultCalculated = false
+    }
     document.getElementById('display').value += value;
 }
 
 function calculateResult() {
     try {
-        document.getElementById('display').value = eval(document.getElementById('display').value);
+        let expression = document.getElementById('display').value;
+        
+        
+        let result = new Function('return ' + expression)();
+        
+        
+        document.getElementById('display').value = result;
+
+        resultCalculated = true;
     } catch {
         document.getElementById('display').value = 'Erro';
+        resultCalculated = true;
     }
 }
 
 function clearDisplay() {
     document.getElementById('display').value = '';
 }
+
